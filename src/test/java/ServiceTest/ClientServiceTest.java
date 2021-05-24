80a88b35a3e0c3815a8e8a7b424ffb2ab2aeb9ff
Дом <s.niyaz@mail.ru>
2021-05-24 16:00:56 +0300

    Rest-test v.1.4

---
+++ src/test/java/ServiceTest/ClientServiceTest.java
@@ -0,0 +1,31 @@
+package ServiceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.safiullin.rest.model.Clients;
+import ru.safiullin.rest.repository.ClientRepository;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+public class ClientServiceTest {
+    @MockBean
+    private ClientRepository clientRepository;
+
+    @Test
+    public void testGetByIdFromDb() {
+        Clients clients = clientRepository.getById(1L);//из базы
+        assertThat(clients.getId()).isEqualTo(1);
+    }
+
+    @Test
+    public void testSaveAndGet() {
+        clientRepository.getById(new Clients("Ivan", "Sidorov", "Mira",1));
+        Clients fromDb = clientRepository.getById(1);
+        assertThat(fromDb.getId()).isEqualTo(1);
+
+    }
+}
+
+
